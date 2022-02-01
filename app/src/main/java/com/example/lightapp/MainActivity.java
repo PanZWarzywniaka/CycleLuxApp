b0a7aa77bbefdfdaa04adcb1276bc79983ec37e0
Aleksander Osikowicz <tupolewcx@gmail.com>
2022-02-01 16:12:37 +0100

    Calculating acceleration score

--- app/src/main/java/com/example/lightapp/MainActivity.java
+++ app/src/main/java/com/example/lightapp/MainActivity.java
@@ -1,15 +1,66 @@
 package com.example.lightapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float dx = event.values[0];
+            float dy = event.values[1];
+            float dz = event.values[2];
+            double score = Math.sqrt(dx*dx + dy*dy + dz*dz);
+            String msg = "Score: "+ score;
+//            String msg = "Values:" +
+//                    "    dx: " + dx +
+//                    "    dy: " + dy +
+//                    "    dz: " + dz;
+            Log.d("EVENT", msg);
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            Log.d("MY_APP", sensor.toString() + " - " + accuracy);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MovementReader mr = new MovementReader();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
+
+
+
+
+
+
